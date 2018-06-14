59e70afaddbbedbb5b41dd69448526e5a1d07506
icarus496 <bendobozy@gmail.com>
2018-06-14 11:16:29 -0400

    added square class

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -5,10 +5,29 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.Pixmap;
 
+class Square
+{
+     //for x and y, top left is origin
+     int squareX; //variable holding the x pos of the square
+     int squareY; //variable holding the y pos of the square
+     int squareColor; //defining the color of the square 0=white, 1=black
+     Square(int squareX, int squareY, int squareColor){
+          this.squareY=squareY;
+          this.squareX=squareX;
+     }
+     int getX(){
+          return squareX;
+     }
+     int getY(){
+          return squareY;
+     }
+}
 public class MyGdxGame extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
+        int squareSize=640/8; //setting size of chessboard squares
 	
 	@Override
 	public void create () {
